f709be43a3be64606714be4543882c500582763d
Bionda <53750621+biondaroest@users.noreply.github.com>
2021-10-27 23:03:04 +0200

    Add files via upload

---
+++ JavaScript_Exercise_011/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten");
+
+// Dit is een grote som.
+/* let antwoordOpdracht1 = 1230941 * 1823794;
+console.log(antwoordOpdracht1);
+
+let antwoordOpdracht2 = 637263 / 54;
+console.log(antwoordOpdracht2); */
