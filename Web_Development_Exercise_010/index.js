31799181462bcff0001762fcd793221dd28c12d8
Bionda <53750621+biondaroest@users.noreply.github.com>
2021-10-27 16:58:41 +0200

    Add files via upload

---
+++ Web_Development_Exercise_010/index.js
@@ -0,0 +1,21 @@
+console.log("Hello Winc Academy");
+let name = "Bionda";
+console.log(name);
+
+let antwoordA = 4 + 4;
+console.log(antwoordA);
+
+let antwoordB = "4" + "4";
+console.log(antwoordB);
+
+let antwoordC = antwoordA / 2;
+console.log(antwoordC);
+
+let antwoordD = 48 * 12 / 6;
+console.log(antwoordD);
+
+let antwoordE = 1 + 1 - 2 / 4 % 9;
+console.log(antwoordE);
+
+let age = "achtentwintig";
+console.log(typeof age);
