1a2daf20acf845b1b1d9de77c24320ad5692147b
Bionda <53750621+biondaroest@users.noreply.github.com>
2021-10-29 13:14:10 +0200

    Add files via upload

---
+++ JavaScript_Exercise_015/script.js
@@ -0,0 +1,39 @@
+const makePizza = function () {
+    console.log("Step 1: Make the dough.");
+    console.log("Step 2: Let the dough rise for 1 hour.");
+    console.log("Step 3: Make the sauce.");
+    console.log("Sidenote: Preheat the oven to 200ºC");
+    console.log("Step 4: Roll out the dough.");
+    console.log("Step 5: Add the souce to the dough with a spoon.");
+    console.log("Step 6: Add your favorite toppings.");
+    console.log("Step 7: Put it in the oven for 10 minutes.");
+    console.log("Step 8: Enjoy your Pizza!");
+    console.log(" ");
+
+}
+
+const makeSushi = function () {
+    console.log("Step 1: Wash the rice.");
+    console.log("Step 2: Preheat water till boil.");
+    console.log("Step 3: Boil the rice for 10 minutes.");
+    console.log("Step 4: Add rice vinegar.");
+    console.log("Step 5: Cut your favorite veggies and fish.");
+    console.log("Step 6: Lay out nori sheets on your bamboo mat and apply rice evenly.");
+    console.log("Step 7: Add precut veggies and fish.");
+    console.log("Step 8: Roll the bamboo tight.");
+    console.log("Step 9: Cut the roll in 6 pieces.");
+    console.log("Step 10: Enjoy your Sushi!");
+    console.log(" ");
+
+}
+
+
+makePizza();
+
+makePizza();
+
+makeSushi();
+
+makeSushi();
+
+makeSushi();
